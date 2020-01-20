44dd1c3e4856c76c665cc4181ad693de24f43311
Doug Hodson <doug@sidechannel.net>
2020-01-20 13:01:54 -0500

    updated game-loop example

--- game-loop/main.cpp
+++ game-loop/main.cpp
@@ -3,10 +3,22 @@
 
 #include <iostream>
 
+Game* game{};
+
 int main() {
 
    std::cout << "Starting game loop\n";
-   
+   game = new Game();
+
+   game->init("1st Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+
+   while (game->running()) {
+      game->handle_events();
+      game->update();
+      game->render();
+   }
+
+   game->clean();
    return 0;
 }
 
